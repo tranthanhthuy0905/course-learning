8033371a7835f59e3e65c7d5c5348cc921bcc780
tranthanhthuy0905 <tranthanhthuylhp2002@gmail.com>
2023-07-12 18:17:50 +0700

    Routers

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// pages
+import App from "./App";
+import Home from "./pages/Home";
+import Setting from "./pages/Setting";
+import Milestones from "./pages/Milestones";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* <Route path="/home" exact element={<Home />} /> */}
+      <Route path="/setting" exact element={<Setting />} />
+      <Route path="/milestones" exact element={<Milestones />} />
+    </Routes>
+  </Router>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 function Home() {
-
   return (
     <div className="home-page">
       <header className="homepage-header"></header>
